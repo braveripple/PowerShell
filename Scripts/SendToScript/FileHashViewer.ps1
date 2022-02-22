b1660d31c23b6e3bfc35c9c611fe1d93d3eb324b
braveripple <braveripple720@gmail.com>
2022-02-23 00:13:03 +0900

    create FileHashViewer

---
+++ Scripts/SendToScript/FileHashViewer.ps1
@@ -0,0 +1,26 @@
+#Requires -Modules BurntToast
+
+$TITLE = "🔍ファイルのハッシュ値を調べる"
+
+# ファイルハッシュをグリッド表示する
+$select = $Args | 
+    ForEach-Object {
+        Get-FileHash -LiteralPath $_ -Algorithm SHA256
+        Get-FileHash -LiteralPath $_ -Algorithm MD5
+        Get-FileHash -LiteralPath $_ -Algorithm SHA1
+    } |
+    Select-Object -Property @{Name="Name";Expression={[System.IO.Path]::GetFileName($_.Path)}}, Algorithm, Hash, Path | 
+    Out-GridView -Title $TITLE -OutputMode Single
+
+# グリッドのデータが選択されたらクリップボードにコピーする
+if ($null -ne $select) {
+    Set-Clipboard $select.Hash
+    New-BurntToastNotification `
+        -Text (
+            "ハッシュ値をクリップボードにコピーしました。`n" +
+            "$($select.Hash)`n" +
+            "・ファイル名:$($select.Name)`n" +
+            "・アルゴリズム:$($select.Algorithm)"
+            ) `
+        -Header (New-BTHeader -Id 1 -Title $TITLE)
+}
