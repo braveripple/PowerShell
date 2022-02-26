f17e2133035b8e95932d201b3f9c4328c3abd02f
braveripple <braveripple720@gmail.com>
2022-02-26 15:47:45 +0900

    削除

--- Scripts/SendToScript/SendToCopy-ItemWithTS.ps1
+++
@@ -1,6 +0,0 @@
-# タイムスタンプを付けてファイルをコピーする
-$resolvePaths = foreach ($arg in $Args) {
-    Get-Item -LiteralPath $arg
-}
-$resolvePaths | 
-../Copy-ItemWithTS.ps1 -Format "FileDateTime"
--- Scripts/SendToScript/Write-Host.ps1
+++
@@ -1,5 +0,0 @@
-# コンソールに表示するだけ
-foreach ($arg in $Args) {
-    Write-Host $arg
-}
-pause
