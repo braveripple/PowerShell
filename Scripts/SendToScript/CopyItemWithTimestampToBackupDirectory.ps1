d54fc0c92e537624e523cdd7f69383dda767b47f
braveripple <braveripple720@gmail.com>
2022-02-27 00:25:53 +0900

    改行を修正

--- Scripts/SendToScript/CopyItemWithTimestampToBackupDirectory.ps1
+++ Scripts/SendToScript/CopyItemWithTimestampToBackupDirectory.ps1
@@ -1,9 +1,12 @@
 # タイムスタンプを付けてファイルを特定のディレクトリにコピーする
-$backupDirectory = "${HOME}/Documents/Backup"
+if ($env:OneDrive) {
+    $backupDirectory = "${HOME}/OneDrive/Documents/Backup"
+} else {
+    $backupDirectory = "${HOME}/Documents/Backup"
+}
 New-Item -Path $backupDirectory -ItemType Directory -Force
 
 $resolvePaths = foreach ($arg in $Args) {
     Get-Item -LiteralPath $arg
 }
-$resolvePaths | 
-../Copy-ItemWithTimestamp.ps1 -Format "yyyyMMddHHmmss" -Destination $backupDirectory
+$resolvePaths | ../Copy-ItemWithTimestamp.ps1 -Format "yyyyMMddHHmmss" -Destination $backupDirectory
