150204b6f9b8cc8de5237c3cfc7f2d8259246132
braveripple <braveripple720@gmail.com>
2022-03-04 00:53:00 +0900

    存在しないファイルのtouchに対応

--- Scripts/Touch-Item.ps1
+++ Scripts/Touch-Item.ps1
@@ -1,5 +1,5 @@
 [CmdletBinding(
-    SupportsShouldProcess = $True,
+    SupportsShouldProcess,
     DefaultParameterSetName = 'Path'
 )]
 param (
@@ -14,21 +14,51 @@ param (
         ValueFromPipeline = $False, ValueFromPipelineByPropertyName = $True)]
     [string[]]$LiteralPath
 )
-begin {
-    write-host "begin"
-}
 process {
+    $targets = @()
     if ($PSBoundParameters.ContainsKey('Path')) {
-        $targets = Convert-Path -Path $Path
+        #Write-Output "Path:${Path}"
+        $Path | ForEach-Object {
+            if (Test-Path -Path $_ -PathType Any) {
+                #Write-Output "P:$(${_}) is exist"
+                $targets += Convert-Path -Path $_
+            } else {
+                #Write-Output "P:$(${_}) is not exist"
+                $parent = Split-Path -Path $_ -Parent
+                $child = Split-Path -Path $_ -Leaf
+                if (Test-Path -LiteralPath $parent -PathType Container) {
+                    $targets += Join-Path -Path (Convert-Path $parent) -ChildPath $child
+                } else {
+                    $targets += Join-Path -Path (Get-Location) -ChildPath $child
+                }
+            }
+        }
     }
     else {
-        $targets = Convert-Path -LiteralPath $LiteralPath
+        #Write-Output "LiteralPath:${LiteralPath}"
+        $LiteralPath | ForEach-Object {
+            if (Test-Path -LiteralPath $_ -PathType Any) {
+                #Write-Output "LP:$(${_}) is exist."
+                $targets += Convert-Path -LiteralPath $_
+            } else {
+                #Write-Output "LP:$(${_}) is not exist."
+                $parent = Split-Path -Path $_ -Parent
+                $child = Split-Path -Path $_ -Leaf
+                if (Test-Path -LiteralPath $parent -PathType Container) {
+                    $targets += Join-Path -Path (Convert-Path $parent) -ChildPath $child
+                } else {
+                    $targets += Join-Path -Path (Get-Location) -ChildPath $child
+                }
+            }
+        }
     }
     $targets | Foreach-Object {
-        if ($PSCmdlet.ShouldProcess($_)) {
+        if ($PSCmdlet.ShouldProcess($_, 'touch')) {
             If (Test-Path -LiteralPath $_ -PathType Any) {
                 (Get-Item -LiteralPath $_).LastWriteTime = Get-Date
                 Get-Item -LiteralPath $_
+            } else {
+                New-Item -Path $_ -ItemType File
             }
         }
     }
