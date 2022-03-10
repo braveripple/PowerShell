90e479d4a8f2007801b821bb15acc1be578df64d
braveripple <braveripple720@gmail.com>
2022-03-10 23:45:45 +0900

    更新日時を付与してリネームを新規作成

---
+++ Scripts/SendToScript/RenameItemWithWriteTime.ps1
@@ -0,0 +1,5 @@
+# アイテムにファイルの更新日時を付ける
+$resolvePaths = foreach ($arg in $Args) {
+    Get-Item -LiteralPath $arg
+}
+$resolvePaths | ../Rename-ItemWithTimestamp.ps1 -Format "yyyyMMddHHmmss" -TimestampType WriteTime
