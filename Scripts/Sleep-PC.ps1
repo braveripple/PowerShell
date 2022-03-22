f9ac5cee69dd91b420d99bbf043cf27d22bf1290
braveripple <braveripple720@gmail.com>
2022-03-22 18:30:08 +0900

    名前変更

--- Scripts/Sleep-PC.ps1
+++
@@ -1,6 +0,0 @@
-$signature = @"
-[DllImport("powrprof.dll")]
-public static extern bool SetSuspendState(bool Hibernate,bool ForceCritical,bool DisableWakeEvent);
-"@
-$func = Add-Type -memberDefinition $signature -namespace "Win32Functions" -name "SetSuspendStateFunction" -passThru
-$func::SetSuspendState($false, $false, $false)
