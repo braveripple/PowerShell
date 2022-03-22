091db8a8b193a3f88e6acb0a07445ee29e14cef0
braveripple <braveripple720@gmail.com>
2022-03-22 19:56:49 +0900

    SupportsShouldProcessに対応

--- Scripts/Remove-Timestamp.ps1
+++ Scripts/Remove-Timestamp.ps1
@@ -1,7 +1,11 @@
 # 文字列からタイムスタンプっぽいものを削除する
+[CmdletBinding(
+    SupportsShouldProcess = $True
+)]
 param (
-    [Parameter(Mandatory = $True, Position = 0)]
-    [string]
+    [Parameter(Mandatory = $True, Position = 0,
+        ValueFromPipeline = $True, ValueFromPipelineByPropertyName = $True)]
+    [string[]]
     $InputString,
     
     [Parameter(Mandatory = $False)]
@@ -12,25 +16,30 @@ param (
     [string]
     $Prefix = "_"
 )
-$formatRegex = $Format -replace "[yMdHms]","\d"
-$dateStrings = [RegEx]::Matches($InputString, $formatRegex)
-$dateStrings | ForEach-Object `
--Begin { 
-    $resultString = $InputString 
-} `
--Process {
-    [ref] $parsedDate = Get-Date
-    $dateString = $_.Value
-    if([DateTime]::TryParseExact(
-        $dateString, 
-        $Format, 
-        [Globalization.DateTimeFormatInfo]::CurrentInfo,
-        [System.Globalization.DateTimeStyles]::None,
-        $parsedDate
-    )) {
-        $resultString = $resultString -replace "${Prefix}${dateString}", ""
+begin {
+    $formatRegex = $Format -replace "[yMdHms]","\d"
+}
+process {
+    foreach($s in $InputString) {
+        $dateStrings = [RegEx]::Matches($s, $formatRegex)
+        $resultString = $s 
+        foreach($dataString in $dateStrings) {
+            [ref] $parsedDate = Get-Date
+            $value = $dataString.Value
+            if([DateTime]::TryParseExact(
+                $value, 
+                $Format, 
+                [Globalization.DateTimeFormatInfo]::CurrentInfo,
+                [System.Globalization.DateTimeStyles]::None,
+                $parsedDate
+            )) {
+                $resultString = $resultString -replace "${Prefix}${value}", ""
+            }
+        }
+        # ShouldProcessで表示する処理対象
+        $targetVerbose = "Before: ${s} After: ${resultString}"
+        if ($PSCmdlet.ShouldProcess($targetVerbose, "Remove Timestamp")) {
+            $resultString
+        }
     }
-} `
--End {
-    $resultString
 }
